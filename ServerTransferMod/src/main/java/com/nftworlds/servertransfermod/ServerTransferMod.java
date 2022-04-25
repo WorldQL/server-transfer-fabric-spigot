08a5e8bab01fa5155c1d17b414fd20fb84f13135
CodedRed <devcodedred@gmail.com>
2022-04-25 15:14:53 -0500

    1.18.2 Update

---
+++ ServerTransferMod/src/main/java/com/nftworlds/servertransfermod/ServerTransferMod.java
@@ -0,0 +1,30 @@
+package com.nftworlds.servertransfermod;
+
+import com.nftworlds.servertransfermod.network.ServerTransferReader;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/***
+ * Developed by @WorldQL
+ *
+ */
+public class ServerTransferMod implements ModInitializer {
+    public static final String ID = "servertransfermod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+    public static Identifier id(String name) {
+        return new Identifier(ID, name);
+    }
+
+    @Override
+    public void onInitialize() {
+        ServerTransferReader reader = new ServerTransferReader();
+        reader.register();
+    }
+
+
+
+}
