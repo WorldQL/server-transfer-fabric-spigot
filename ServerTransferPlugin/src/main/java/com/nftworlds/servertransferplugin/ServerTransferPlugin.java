c7b55b9c0e7f90f5689ff3c5f681cb9b36f00bb4
videogame hacker <half-kh-hacker@hackery.site>
2022-05-02 16:32:44 +0100

    (Untested) Switch to reflection instead of direct CraftPlayer cast

--- ServerTransferPlugin/src/main/java/com/nftworlds/servertransferplugin/ServerTransferPlugin.java
+++ ServerTransferPlugin/src/main/java/com/nftworlds/servertransferplugin/ServerTransferPlugin.java
@@ -3,17 +3,19 @@ package com.nftworlds.servertransferplugin;
 import com.google.common.base.Preconditions;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
-import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Method;
+
 
 public final class ServerTransferPlugin extends JavaPlugin {
+    private static final String WQL_CHANNEL = "servertransfermod:wql_channel";
 
     @Override
     public void onEnable() {
-        if (!getServer().getMessenger().getOutgoingChannels().contains("servertransfermod:wql_channel"))
-            getServer().getMessenger().registerOutgoingPluginChannel(this, "servertransfermod:wql_channel");
+        if (!getServer().getMessenger().getOutgoingChannels().contains(WQL_CHANNEL))
+            getServer().getMessenger().registerOutgoingPluginChannel(this, WQL_CHANNEL);
 
     }
 
@@ -28,12 +30,16 @@ public final class ServerTransferPlugin extends JavaPlugin {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF(ip_address);
 
-        CraftPlayer craftPlayer = (CraftPlayer) player;
+        if (!player.getListeningPluginChannels().contains(WQL_CHANNEL)) {
+            try {
+                Method method = player.getClass().getDeclaredMethod("addChannel", String.class);
+                method.invoke(player, WQL_CHANNEL);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
-        if (!player.getListeningPluginChannels().contains("servertransfermod:wql_channel"))
-            craftPlayer.addChannel("servertransfermod:wql_channel");
-
-        player.sendPluginMessage(ServerTransferPlugin.getPlugin(ServerTransferPlugin.class), "servertransfermod:wql_channel", out.toByteArray());
+        player.sendPluginMessage(ServerTransferPlugin.getPlugin(ServerTransferPlugin.class), WQL_CHANNEL, out.toByteArray());
 
     }
 
